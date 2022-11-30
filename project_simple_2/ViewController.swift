97c483f4d93f723b228a93bf132d02f2fc8a1dab
levi <tdnam.cmc@gmail.com>
2022-11-30 09:18:31 +0700

    Initial Commit

---
+++ project_simple_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project_simple_2
+//
+//  Created by Nam on 30/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
